1a982ec29d47c05490954cc3d66c6c9d412ccd73
Przemek <przemekworld@gmail.com>
2016-04-30 12:05:53 +0200

    Bindowanie IDictionary<T,T> w MVC 5

---
+++ WebMvcStudy/Controllers/FirstController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMvcStudy.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        public ActionResult Index()
+        {
+            var dictModel = new Dictionary<string, string>
+            {
+                { "Przemek","Walkowski" },
+                { "Jola", "Grzybowska" }
+            };
+
+            return View(dictModel);
+        }
+
+        public ActionResult SecondAction(IDictionary<string,string> lista)
+        {
+            return View(lista);
+        }
+    }
+}
