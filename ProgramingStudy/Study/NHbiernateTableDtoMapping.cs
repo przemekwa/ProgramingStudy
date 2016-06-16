a21650577f1ecb7ceb2b4b3a11d681612c7745cf
Przemek <przemekworld@gmail.com>
2016-06-16 17:47:08 +0200

    Dodanie NHibernate i dodanie mapowania

---
+++ ProgramingStudy/Study/NHbiernateTableDtoMapping.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+
+namespace ProgramingStudy.Study
+{
+    class NHbiernateTableDtoMapping : ClassMapping<NHbiernateTableDto>
+    {
+        public NHbiernateTableDtoMapping()
+        {
+            base.Id(x => x.Id, map =>
+              {
+                  map.Column("Serno");
+                  map.Generator(Generators.Native);
+                  map.UnsavedValue(0);
+              });
+
+            base.Property(x => x.Name, map =>
+              {
+                  map.Column("Name");
+              });
+        }
+    }
+}
