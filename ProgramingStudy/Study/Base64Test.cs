29006f27f0fda4decc7d33a749a1bcf4951a78a6
Walkowski Przemysław <127356@centrala.bzwbk>
2016-07-27 08:41:02 +0200

    Dodanie klasy z Base64

---
+++ ProgramingStudy/Study/Base64Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProgramingStudy.Study
+{
+    class Base64Test : IStudyTest
+    {
+        public void Study()
+        {
+            var stringTest = "Idealy sa jak gwiazdy - nie mozna ich osiagnac, ale mozna sie nimi kierowac.";
+
+            var result = Convert.ToBase64String(Encoding.ASCII.GetBytes(stringTest), Base64FormattingOptions.InsertLineBreaks);
+
+            Console.WriteLine(result);
+
+            Console.WriteLine(Encoding.ASCII.GetString(Convert.FromBase64String(result)));
+
+            AppDomain.CurrentDomain.SetPrincipalPolicy(PrincipalPolicy.WindowsPrincipal);
+
+            var t = Thread.CurrentPrincipal;
+
+            var bf = new BinaryFormatter();
+
+            using (var ms = new MemoryStream())
+            {
+                bf.Serialize(ms, t);
+
+                var base64 = Convert.ToBase64String(ms.GetBuffer());
+            }
+            
+            //using (var ms = new FileStream("userPrincipial.dat", FileMode.Open))
+            //{
+            //    var result2 = (WindowsIdentity)bf.Deserialize(ms);
+            //               }
+        }
+    }
+}
