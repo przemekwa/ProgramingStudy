59e065e5deddc779990d31b3ec6505bc32bb8bb1
Walkowski Przemysław <127356@centrala.bzwbk>
2016-07-27 09:23:39 +0200

    Dodanie klasy z Mazowia

---
+++ ProgramingStudy/Study/MazowiaConverterFromTuxedo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramingStudy.Study
+{
+    public static class MazowiaConverterFromTuxedo
+    {
+        public enum EncodingTuxedoType
+        {
+            PL_ISO = 0,
+            PL_WIN,
+            PL_LTN,
+            PL_MZV,
+            PL_ATM,
+            PL_BEZ
+        }
+
+        private static int[,] lc_tab_pl =
+        {
+            {161,198,202,163,209,211,166,172,175,177,230,234,179,241,243,182,188,191},
+            {165,198,202,163,209,211,140,143,175,185,230,234,179,241,243,156,159,191},
+            {164,143,168,157,227,224,151,141,189,165,134,169,136,228,162,152,171,190},
+            {143,149,144,156,165,163,152,160,161,134,141,145,146,164,162,158,166,167},
+            {196,199,203,208,209,211,214,218,220,228,231,235,240,241,243,246,250,252},
+            { 65, 67, 69, 76, 78, 79, 83, 90, 90, 97, 99,101,108,110,111,115,122,122}
+        };
+
+
+        public static byte[] FrzKonwPL(byte[] toConvertString, EncodingTuxedoType iZ, EncodingTuxedoType iDo)
+        {
+            if (iZ < 0 || (short)iZ > 5 || (short)iDo < 0 || (short)iDo > 5)
+            {
+                return new byte[0];
+            }
+
+            for (var i = 0; i < toConvertString.Length; i++)
+            {
+                for (var j = 0; j < 18; j++)
+                {
+                    if (toConvertString[i] == lc_tab_pl[(short)iZ, j])
+                    {
+                        toConvertString[i] = (byte)lc_tab_pl[(short)iDo, j];
+                        break;
+                    }
+                }
+            }
+
+            return toConvertString;
+        }
+    }
+}
---
+++ ProgramingStudy/Study/MazowiaEncoding.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramingStudy.Study
+{
+    class MazowiaEncoding : IStudyTest
+    {
+        public void Study()
+        {
+            string[] stringTab =
+            {
+                "ą", "ć", "ę", "ł", "ń", "ó", "ś", "ź", "ż","ţ"
+            };
+
+            byte[] byteMazoviaCheck =
+            {
+                134, 141, 145, 146, 164, 162, 158, 166, 167,254
+            };
+
+
+            for (int index = 0; index < stringTab.Length; index++)
+            {
+                var temp =
+                    MazowiaConverterFromTuxedo.FrzKonwPL(Encoding.GetEncoding("iso-8859-2").GetBytes(stringTab[index]),
+                        MazowiaConverterFromTuxedo.EncodingTuxedoType.PL_ISO,
+                        MazowiaConverterFromTuxedo.EncodingTuxedoType.PL_MZV);
+
+                if (byteMazoviaCheck[index] != temp[0])
+                {
+                    throw new Exception();
+                }
+
+
+
+                var temp2 = MazowiaConverterFromTuxedo.FrzKonwPL(temp,
+                        MazowiaConverterFromTuxedo.EncodingTuxedoType.PL_MZV,
+                        MazowiaConverterFromTuxedo.EncodingTuxedoType.PL_ISO);
+
+
+                if (temp2[0] != (Encoding.GetEncoding("iso-8859-2").GetBytes(stringTab[index]))[0])
+                {
+                    throw new Exception();
+                }
+
+                var t = Encoding.GetEncoding("iso-8859-2").GetString(temp2);
+
+                if (t == "ţ")
+                {
+                    throw new Exception();
+                }
+            }
+        }
+    }
+   
+}
