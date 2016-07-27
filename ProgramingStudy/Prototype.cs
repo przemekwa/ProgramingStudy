9cc0993bc706a1bdc3ee2429bfef42ff8f2e75d5
Walkowski Przemysław <127356@centrala.bzwbk>
2016-07-27 10:21:18 +0200

    Dodanie FizzBuzz

---
+++ ProgramingStudy/Prototype.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramingStudy
+{
+    public class Prototyp : ICloneable
+    {
+        public Point PolozeniePoint { get; set; }
+
+        public Point PolozeniePunkt { get; set; }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+
+    public class TestPrototyp
+    {
+        public TestPrototyp()
+        {
+            var obj1 = new Prototyp
+            {
+                PolozeniePoint = new Point(0, 0),
+                PolozeniePunkt = new Point(1, 1)
+            };
+
+
+            var obj2 = (Prototyp)obj1.Clone();
+        }
+    }
+}
---
+++ ProgramingStudy/Study/FizzBuzz.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramingStudy.Study
+{
+    public class FizzBuzz : IStudyTest
+    {
+        public void Study()
+        {
+            var number = 15;
+
+            var p3 = number % 3 == 0;
+            var p5 = number % 5 == 0;
+
+            var t = new List<Object>
+            {
+                number%3 == 0,
+                "Fizz",
+                number%5 == 0,
+                "Buzz",
+            };
+
+            var result = string.Empty;
+
+            for (var i = 0; i < t.Count; i = i + 2)
+            {
+                if ((bool)t[i])
+                {
+                    result += t[i + 1];
+                }
+            }
+
+            if (string.IsNullOrEmpty(result))
+            {
+                result = number.ToString();
+            }
+
+            Console.WriteLine(result);
+
+
+            if (p3 && p5)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (p3)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (p5)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
