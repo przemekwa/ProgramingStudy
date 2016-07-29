f6841f34b973949ff7c79a1b99ded1fdaaad6afd
przemekwa <przemyslaw.walkowski@bzwbk.pl>
2016-07-29 08:28:38 +0200

    Dołożenie interfejsu INotifyPropertyChanged

--- ColorPreview/ViewModel/ColorViewModel.cs
+++ ColorPreview/ViewModel/ColorViewModel.cs
@@ -1,21 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using ColorPreview.Annotations;
 using ColorPreview.Model;
 
 namespace ColorPreview.ViewModel
 {
-    public class ColorViewModel
+    public class ColorViewModel : INotifyPropertyChanged
     {
-        public Color Color { get; set; }
+        private Color color;
+
+        public Color Color
+        {
+            get { return color; }
+            set
+            {
+                color = value;
+                this.OnPropertyChanged("Color");
+            }
+        }
+
 
         public int IncrementValue { get; set; } = 23;
 
         public ColorViewModel()
         {
             this.Color = UserSettings.GetUserColor();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        protected virtual void OnPropertyChanged(params string[] propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                foreach (var p in propertyName)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(p));
+                }
+            }
         }
     }
 }
