4d910f432126b1050a84c3af0ef6decf2177f645
przemekwa <przemyslaw.walkowski@bzwbk.pl>
2016-07-29 08:31:41 +0200

    Być może Color nie potrzebuje interfejsu INotifiPropertyChanged

--- ColorPreview/Model/Color.cs
+++ ColorPreview/Model/Color.cs
@@ -1,76 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using ColorPreview.Annotations;
 
 namespace ColorPreview.Model
 {
-    public class Color : INotifyPropertyChanged
+    public class Color
     {
-
-        private byte r;
-        private byte g;
-        private byte b;
-
-        public byte R
-        {
-            get
-            {
-                return r;
-            }
-
-            set
-            {
-                r = value;
-                OnPropertyChanged("R");
-            }
-        }
-
-
-        public byte G
-        {
-            get
-            {
-                return g;
-            }
-
-            set
-            {
-                g = value;
-                OnPropertyChanged("G");
-            }
-        }
-
-        public byte B
-        {
-            get
-            {
-                return b;
-            }
-
-            set
-            {
-                b = value;
-                OnPropertyChanged("B");
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-
-        protected virtual void OnPropertyChanged(params string[] propertyName)
-        {
-            if (PropertyChanged != null)
-            {
-                foreach (var p in propertyName)
-                {
-                    PropertyChanged(this, new PropertyChangedEventArgs(p));
-                }
-            }
-        }
+        public byte R { get; set; }
+        public byte G { get; set; }
+        public byte B { get; set; }
     }
 }
