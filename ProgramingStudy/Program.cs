d7a0985c2b524d3a61f7d264c643a8e7f4b8488f
127356 <127356@L716540>
2016-08-05 11:43:26 +0200

    Przykład wzorca observer

--- ProgramingStudy/Program.cs
+++ ProgramingStudy/Program.cs
@@ -11,7 +11,7 @@ namespace ProgramingStudy
 {
     public class Program
     {
-        public static readonly IStudyTest StudyTest = new ThinkInterview();
+        public static readonly IStudyTest StudyTest = new ObserverrDesignPatternStudy();
 
         public static void Main(string[] args)
         { 
---
+++ ProgramingStudy/Study/ObserverrDesignPatternStudy.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProgramingStudy.Study
+{
+    class ObserverrDesignPatternStudy : IStudyTest
+    {
+        public void Study()
+        {
+
+            var investor1 = new Investor("Przemek");
+            var investor2 = new Investor("Jola");
+
+            var bzwbk = new ConcreteStock();
+
+            bzwbk.Attach(investor1);
+            bzwbk.Attach(investor2);
+
+            bzwbk.Price = 100;
+
+            Thread.Sleep(3000);
+
+            bzwbk.Price = 200;
+
+            Thread.Sleep(3000);
+
+            bzwbk.Price = 300;
+
+
+
+        }
+
+
+
+
+
+    }
+
+
+    public class ConcreteStock : Stock
+    {
+        private decimal price;
+
+        public decimal Price
+        {
+            get { return price; }
+            set
+            {
+                price = value;
+                this.NotifyAll(value);
+            }
+        }
+    }
+
+
+    public abstract class Stock
+    {
+        public List<Investor> Investros { get; set; } = new List<Investor>();
+
+        public void Attach(Investor i)
+        {
+            this.Investros.Add(i);
+        }
+
+        public void Detach(Investor i)
+        {
+            this.Investros.Remove(i);
+        }
+
+        public virtual void NotifyAll(decimal price)
+        {
+            foreach (var investro in this.Investros)
+            {
+                investro.Show(price);
+            }
+        }
+
+    }
+
+    public class Investor
+    {
+        public string Name { get; set; }
+
+        public Investor(string name)
+        {
+            Name = name;
+        }
+
+        public void Show(decimal price)
+        {
+            Console.WriteLine($" {Name} widzi zmiane ceny na {price}");
+        }
+    }
+}
