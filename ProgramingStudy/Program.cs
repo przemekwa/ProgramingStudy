eeb4c4c55882e43192d010a160d39c92156b5ed2
przemekwa <przemyslaw.walkowski@bzwbk.pl>
2016-09-16 13:13:31 +0200

    Serializowanie JSON

--- ProgramingStudy/Program.cs
+++ ProgramingStudy/Program.cs
@@ -11,7 +11,7 @@ namespace ProgramingStudy
 {
     public class Program
     {
-        public static readonly IStudyTest StudyTest = new SoupEnvelopeCreator();
+        public static readonly IStudyTest StudyTest = new SerializationJSON();
 
         public static void Main(string[] args)
         { 
---
+++ ProgramingStudy/Study/SerializationJSON.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace ProgramingStudy.Study
+{
+    public class SerializationJSON : IStudyTest
+    {
+        public void Study()
+        {
+
+            var testObj = new CreateInstalmentArgument();
+            
+            var result = "";
+
+            result = JsonConvert.SerializeObject(testObj);
+
+
+            var @object = JsonConvert.DeserializeObject<CreateInstalmentArgument>(result);
+
+
+        }
+    }
+}
