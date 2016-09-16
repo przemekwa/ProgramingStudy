34141a9a0975608afa78cbd1431cfdc94cc33006
przemekwa <przemyslaw.walkowski@bzwbk.pl>
2016-09-16 11:26:38 +0200

    Produkowanie dzia³aj¹cych w SOUPUI koberty do Primea

--- ProgramingStudy/Program.cs
+++ ProgramingStudy/Program.cs
@@ -11,7 +11,7 @@ namespace ProgramingStudy
 {
     public class Program
     {
-        public static readonly IStudyTest StudyTest = new Serialization();
+        public static readonly IStudyTest StudyTest = new SoupEnvelopeCreator();
 
         public static void Main(string[] args)
         { 
---
+++ ProgramingStudy/Study/SoupEnvelopeCreator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ProgramingStudy.Study
+{
+   
+    public class CreateInstalmentArgument
+    {
+        public int AccountId { get; set; }
+        public decimal LoanAmount { get; set; }
+        public int LoanProfileId { get; set; }
+        public List<int> InputSchedule { get; set; }
+        public string UserName { get; set; }
+        public bool RealTimePosting { get; set; }
+        public bool IsValidationEnabled { get; set; }
+        public string Reason { get; set; }
+        public string CorrelationId { get; set; }
+        public string UserId { get; set; }
+
+    }
+
+    [XmlType(Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+    [XmlRoot(ElementName = "Envelope", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+    public class SOAPEnvelope
+    {
+        [XmlAttribute(AttributeName = "soapenv", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+        public string soapenva { get; set; }
+
+        [XmlAttribute(AttributeName = "xsd", Namespace = "http://www.w3.org/2001/XMLSchema")]
+        public string xsd { get; set; }
+
+        [XmlAttribute(AttributeName = "xsi", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string xsi { get; set; }
+
+        [XmlElement(ElementName = "Body", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+        public ResponseBody<CreateInstalmentArgument> body { get; set; }
+
+        [XmlNamespaceDeclarations]
+        public XmlSerializerNamespaces xmlns = new XmlSerializerNamespaces();
+        public SOAPEnvelope()
+        {
+            xmlns.Add("soapenv", "http://schemas.xmlsoap.org/soap/envelope/");
+            xmlns.Add("prim", "http://bzwbk.com/services/prime/");
+        }
+    }
+
+    [XmlRoot(ElementName = "Body", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+    public class ResponseBody<T>
+    {
+        [XmlElement(Namespace = "http://bzwbk.com/services/prime/")]
+        public T CreateInstalment { get; set; }
+    }
+
+
+    class SoupEnvelopeCreator : IStudyTest
+    {
+        public void Study()
+        {
+            var se = new SOAPEnvelope
+            {
+                body = new ResponseBody<CreateInstalmentArgument>
+                {
+                    CreateInstalment = new CreateInstalmentArgument { UserName = "Przemek"}
+                }
+            };
+
+            var soapserializer = new XmlSerializer(typeof(SOAPEnvelope));
+
+            using (var ms = new MemoryStream())
+            {
+                soapserializer.Serialize(ms, se);
+
+                var result = Encoding.UTF8.GetString(ms.ToArray());
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
