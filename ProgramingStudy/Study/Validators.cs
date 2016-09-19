760a227e12ab5fe609e2ed498deb62ee6e5cdd0c
przemekwa <przemyslaw.walkowski@bzwbk.pl>
2016-09-19 14:19:20 +0200

    Inne Validatory

--- ProgramingStudy/Study/Validators.cs
+++ ProgramingStudy/Study/Validators.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,91 @@ namespace ProgramingStudy.Study
 {
     public static class Validator
     {
+        /// <summary>
+        /// Check if NIP is valid. Alroritm on http://pl.wikipedia.org/wiki/NIP
+        /// </summary>
+        /// <param name="nip">Value of Nip</param>
+        /// <returns><c>true</c> if [is valid NIP] [the specified NIP]; otherwise, <c>false</c>.</returns>
+        public static bool IsValidNip(string nip)
+        {
+            if (string.IsNullOrEmpty(nip))
+            {
+                throw new ArgumentNullException("nip");
+            }
+
+            var nipToCheck = nip.Replace("-", "").Replace(" ", "").Trim();
+
+            if (nipToCheck.Length != 10)
+            {
+                return false;
+            }
+
+            var wages = new[]
+            {
+                6, 5, 7, 2, 3, 4, 5, 6, 7
+            };
+
+            var digtArrays = nipToCheck.Select(d => int.Parse(d.ToString(CultureInfo.InvariantCulture))).ToArray();
+
+            var sum =
+                digtArrays
+                    .Take(9)
+                    .Select((digt, index) => new { number = digt, wage = index })
+                    .Sum(digt => digt.number * wages[digt.wage]);
+
+            return digtArrays[9] == (sum % 11);
+        }
+
+        /// <summary>
+        /// Check if REGON is valid. Alroritm on https://pl.wikipedia.org/wiki/REGON
+        /// </summary>
+        /// <param name="regon">Regon number</param>
+        /// <returns><c>true</c> if [is valid REGON] [the specified REGON]; otherwise, <c>false</c>.</returns>
+        public static  bool IsValidRegon(string regon)
+        {
+            if (string.IsNullOrEmpty(regon))
+            {
+                return false;
+            }
+
+            int[] wages;
+            int controlNumberFromRegon = 0;
+            int controlNumber;
+
+            switch (regon.Length)
+            {
+                case 9:
+                    wages = new[] { 8, 9, 2, 3, 4, 5, 6, 7 };
+
+                    for (int i = 0; i < 8; i++)
+                    {
+                        controlNumberFromRegon += int.Parse(regon[i].ToString()) * wages[i];
+                    }
+
+                    controlNumber = controlNumberFromRegon % 11;
+
+                    if (controlNumber == 10)
+                    {
+                        controlNumber = 0;
+                    }
+
+                    return controlNumber.ToString() == regon[8].ToString();
+                case 14:
+                    wages = new[] { 2, 4, 8, 5, 0, 9, 7, 3, 6, 1, 2, 4, 8 };
+
+                    for (int i = 0; i < 13; i++)
+                    {
+                        controlNumberFromRegon += int.Parse(regon[i].ToString()) * wages[i];
+                    }
+
+                    controlNumber = controlNumberFromRegon % 11;
+
+                    return controlNumber.ToString() == regon[13].ToString();
+                default:
+                    return false;
+            }
+        }
+
         public static bool IsPeselValid(string pesel)
         {
             if (pesel == null || pesel.Length != 11)
--- ProgramingStudyTests/Validators_Tests.cs
+++ ProgramingStudyTests/Validators_Tests.cs
@@ -10,10 +10,53 @@ namespace ProgramingStudyTests
 {
     public class Validators_Tests
     {
-        [Fact]
-        public void Pesel_Validator_Test()
+        [Theory]
+        [InlineData("00281802650")]
+        public void IsValidPesel(string pesel)
         {
-            Assert.True(Validator.IsPeselValid("00281802650"));
+            Assert.True(Validator.IsPeselValid(pesel));
+        }
+
+        [Theory]
+        [InlineData("930041341")]
+        [InlineData("141387142")]
+        [InlineData("839072903")]
+        [InlineData("210466948")]
+        [InlineData("23511332857188")]
+        public void IsValidRegon(string regon)
+        {
+            Assert.True(Validator.IsValidRegon(regon));
+        }
+
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("123")]
+        [InlineData("1234567890")]
+        [InlineData(null)]
+        public void IsNotValidRegon(string regon)
+        {
+            Assert.False(Validator.IsValidRegon(regon));
+        }
+
+        [Theory]
+        [InlineData(" 527-26-38-392")]
+        [InlineData("1070010731")]
+        [InlineData("896 000 56 73")]
+        [InlineData("123-456-32-18")]
+        public void ValidNip(string nip)
+        {
+            Assert.True(Validator.IsValidNip(nip));
+        }
+
+        [Theory]
+        [InlineData("1234")]
+        [InlineData("6577654567")]
+        [InlineData("1535658590")]
+        [InlineData("1234567890")]
+        public void IsNotValidNip(string nip)
+        {
+            Assert.False(Validator.IsValidNip(nip));
         }
     }
 }
