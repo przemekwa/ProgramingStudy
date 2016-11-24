3e70c665c5c53f115547635170f0bc9751d4c0ee
przemekwa <przemyslaw.walkowski@bzwbk.pl>
2016-11-24 15:26:07 +0100

    Kolejne

--- ProgramingStudy/Study/Kata/CodeWars.cs
+++ ProgramingStudy/Study/Kata/CodeWars.cs
@@ -30,7 +30,67 @@ namespace ProgramingStudy.Study.Kata
     {
         public void Study()
         {
-            Console.WriteLine(DuplicateCount("Indivisibility"));
+            Console.WriteLine(ReverseWords("The greatest victory is that which requires no battle"));
+        }
+
+
+        public static string ReverseWords(string str)
+        {
+            
+            return string.Join(" ", str.Split(' ').Reverse());
+        }
+
+
+
+        //        A child plays with a ball on the nth floor of a big building the height of which is known
+
+        //(float parameter "h" in meters, h > 0) .
+
+        //He lets out the ball.The ball rebounds for example to two-thirds
+
+        //(float parameter "bounce", 0 < bounce< 1)
+
+        //of its height.
+
+        //His mother looks out of a window that is 1.5 meters from the ground
+
+        //(float parameters window<h).
+
+        //How many times will the mother see the ball either falling or bouncing in front of the window
+
+        //(return a positive integer unless conditions are not fulfilled in which case return -1) ?
+
+        //Note
+
+        //You will admit that the ball can only be seen if the height of the rebouncing ball is stricty greater than the window parameter.
+
+        //Example:
+
+        //h = 3, bounce = 0.66, window = 1.5, result is 3
+
+        //h = 3, bounce = 1, window = 1.5, result is -1
+        public static int BouncingBall(double h, double bounce, double window)
+        {
+            var result = 0;
+
+            var currentH = h;
+
+            while (currentH > window)
+            {
+                if (currentH >= window)
+                {
+                    result += 1;
+                }
+
+                currentH = currentH*bounce;
+
+                if (currentH >= window)
+                {
+                    result += 1;
+                }
+            }
+
+            return result == 0 ?-1:result;
         }
 
 
