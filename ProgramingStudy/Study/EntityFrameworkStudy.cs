28bff6eef311dcbb85e8b60337413810e8c2ef00
Przemysław Walkowski <przemyslaw.walkowski@bccgroup.com>
2017-02-24 15:36:00 +0100

    En

---
+++ ProgramingStudy/Study/EntityFrameworkStudy.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramingStudy.Study
+{
+    public class EntityFrameworkStudy : IStudyTest
+    {
+        public void Study()
+        {
+            var context = new EntityContext();
+
+            // context.EntityParents.Add(new EntityParent
+            // {
+            //     Name = "Przemek3",
+            //     EntityChild = new EntityChild
+            //     {
+            //         Name = "Julek3"
+            //     }
+
+            // });
+
+
+            var f = context.EntityParents.First();
+
+            var d = f.EntityChild;
+
+            context.EntityChild.Remove();
+
+            // context.EntityParents.Remove(f);
+
+
+
+
+            context.SaveChanges();
+
+        }
+    }
+
+
+    public class EntityContext : DbContext
+    {
+        public DbSet<EntityParent> EntityParents { get; set; }
+        public DbSet<EntityChild> EntityChild { get; set; }
+
+        public EntityContext() :base("entityFramework")
+        {
+            this.Database.CreateIfNotExists();
+        }
+
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+
+            modelBuilder.Entity<EntityParent>()
+                .HasRequired(a => a.EntityChild)
+                .WithMany()
+                .HasForeignKey(u => u.EntityChildId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+    }
+
+
+    public class EntityParent
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int EntityChildId { get; set; }
+        public virtual EntityChild EntityChild { get; set; }
+    }
+
+    public class EntityChild
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
