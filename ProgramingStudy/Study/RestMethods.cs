8b5c0f7f6a011bcc818864ce42c599dfb7df5e4d
Przemek <przemyslaw.walkowski@bccgroup.com>
2017-06-07 14:29:30 +0200

    Metody wywoływana REST API

---
+++ ProgramingStudy/Study/RestMethods.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramingStudy.Study
+{
+    class RestMethods
+    {
+        public void HttpClient()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 58.0.3029.110 Safari / 537.36");
+                
+                var response = httpClient.GetStringAsync(new Uri(url)).Result;
+
+                var releases = JArray.Parse(response);
+            }
+        }
+
+        public void WebClinet()
+        {
+            var client = new WebClient();
+            client.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36(KHTML, like Gecko) Chrome / 58.0.3029.110 Safari / 537.36");
+            
+
+            var response = client.DownloadString("https://api.github.com/repos/restsharp/restsharp/releases");
+
+            var releases = JArray.Parse(response);
+        }
+
+        public void HttpWebRequest()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.github.com/repos/restsharp/restsharp/releases");
+
+            request.Method = "GET";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 58.0.3029.110 Safari / 537.36";
+            request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            string content = string.Empty;
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader sr = new StreamReader(stream))
+                {
+                    content = sr.ReadToEnd();
+                }
+            }
+
+            var releases = JArray.Parse(content);
+        }
+    }
+}
