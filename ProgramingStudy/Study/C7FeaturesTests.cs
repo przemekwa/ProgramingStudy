3477dbd9c530cd009997f4a616632ce8b1511fcc
Przemek <przemyslaw.walkowski@bccgroup.com>
2017-07-24 11:13:08 +0200

    Dalsza praca

---
+++ ProgramingStudy/Study/C7FeaturesTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramingStudy.Study
+{
+    public class C7FeaturesTests
+    {
+        public C7FeaturesTests(int a)
+        {
+            var s = 4;
+
+            switch (s)
+            {
+                case int s:
+                    break;
+            }
+        }
+    }
+}
--- ProgramingStudy/Study/RestMethods.cs
+++ ProgramingStudy/Study/RestMethods.cs
@@ -18,7 +18,7 @@ namespace ProgramingStudy.Study
             {
                 httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 58.0.3029.110 Safari / 537.36");
                 
-                var response = httpClient.GetStringAsync(new Uri(url)).Result;
+                var response = httpClient.GetStringAsync(new Uri("https://api.github.com/repos/restsharp/restsharp/releases")).Result;
 
                 var releases = JArray.Parse(response);
             }
