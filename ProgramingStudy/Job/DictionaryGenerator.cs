a40fea7d840f093d8f0ab547cff10426d8a7bebe
Przemek <przemyslaw.walkowski@bccgroup.com>
2018-02-16 13:24:10 +0100

    Do pracy

--- ProgramingStudy/Job/DictionaryGenerator.cs
+++ ProgramingStudy/Job/DictionaryGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,18 +11,37 @@ namespace ProgramingStudy.Job
     {
         public void Study()
         {
+            var lines = File.ReadAllLines("");
 
-            var result = MakeFullEntry("1741-011", "F");
+            var dictionaryEntrys = new List<string>();
+            var translationEntrys = new List<string>();
+
+            foreach (var line in lines)
+            {
+                var values = line.Split(new[] { "|" }, StringSplitOptions.RemoveEmptyEntries);
+
+    
+                dictionaryEntrys.Add(MakeFullEntry(values[0], values[1], values[2], values[3]));
+
+
+            }
+
+
+
+                var result = MakeFullEntry("1756-110", "Nie sprecyzowane (gazetka wew., itd…..)", "Usługi marketingowe lub wezwanie", "H110");
 
         }
 
-        public string MakeFullEntry(string accountNumber, string food)
+        public string MakeFullEntry(string accountNumber, string accoutDesc, string textOnInvose, string code)
         {
+            var food = accountNumber.Substring(3, 1) == "1" ? "F" : "NF";
+
             var accoutNameWithDash = accountNumber.Replace("-", "_");
             var accoutNameWithDot = accountNumber.Replace("-", ".");
 
             var sb = new StringBuilder();
-
+            sb.AppendLine("-- START New Additional Settlements Account Dictionaries Entry");
+            sb.AppendLine();
             sb.AppendLine($"--  Additional Settlements Food Account {accountNumber}");
             sb.AppendLine();
             sb.AppendLine($"EXEC [BCC_PG].[AddDictionaryDefinition]");
@@ -33,7 +53,20 @@ namespace ProgramingStudy.Job
             sb.AppendLine($"N'Additional_Settlements_Account_{food}_{accoutNameWithDash}',");
 
             sb.AppendLine($"N'Dictionary.Additional.Settlements.Account.{food}.{accoutNameWithDot}',");
-            sb.AppendLine("'{6AD11ACC-492F-4693-B181-D299AC2259DB}' -- Additional Settlements For Food Account");
+
+
+            if (food == "F")
+            {
+                sb.AppendLine("'{6AD11ACC-492F-4693-B181-D299AC2259DB}' -- Additional Settlements For Food Account");
+
+            }
+            else
+            {
+                sb.AppendLine("'{6530E779-C097-492E-BB7E-4AE8943B039C}' -- Additional Settlements For Non Food Account");
+            }
+
+
+
             sb.AppendLine();
             sb.AppendLine($"EXEC[BCC_PG].[AddDictionaryEntry]");
 
@@ -51,15 +84,48 @@ namespace ProgramingStudy.Job
             sb.AppendLine($"'{{{texyOnInvoiceId}}}',");
             sb.AppendLine($"N'Additional_Settlements_TextOnInvoice_{food}_{accoutNameWithDash}',");
             sb.AppendLine($"N'Dictionary.Additional.Settlements.TextOnInvoice.{food}.{accoutNameWithDot}',");
-            sb.AppendLine($"'{{{accoutId}}}',  -- Account {accountNumber}");
-            sb.AppendLine($"");
+            sb.AppendLine($"'{{{accoutId}}}'  -- Account {accountNumber}");
+            sb.AppendLine($""); 
             sb.AppendLine($" EXEC [BCC_PG].[AddDictionaryEntry] ");
             sb.AppendLine($"'{{{Guid.NewGuid()}}}',");
             sb.AppendLine($"'{{{texyOnInvoiceId}}}',");
             sb.AppendLine($"N'Dictionary.Additional.Settlements.TextOnInvoice.{food}.{accoutNameWithDot}.1',");
-            sb.AppendLine($"N'1");
+            sb.AppendLine($"N'1'");
+
+            sb.AppendLine($"");
+
+            sb.AppendLine($"--  Additional Settlements Food Account {accountNumber} Code");
+            sb.AppendLine($"");
+            sb.AppendLine($"EXEC[BCC_PG].[AddDictionaryDefinition]");
+            var codeId = Guid.NewGuid();
+            sb.AppendLine($"'{{{codeId}}}',");
+            sb.AppendLine($"N'Additional_Settlements_Account_{food}_{accoutNameWithDash}_Code',");
+            sb.AppendLine($"N'Dictionary.Additional.Settlements.Account.{food}.{accoutNameWithDot}',");
+            sb.AppendLine($"null");
+            sb.AppendLine($"");
+            sb.AppendLine($"EXEC [BCC_PG].[AddDictionaryEntry] ");
+            sb.AppendLine($"'{{{Guid.NewGuid()}}}',");
+            sb.AppendLine($"'{{{codeId}}}',");
+            sb.AppendLine($"N'Dictionary.Additional.Settlements.Account.{food}.{accoutNameWithDot}',");
+            sb.AppendLine($"N'{code}'");
+            sb.AppendLine("-- STOP New Additional Settlements Account Dictionaries Entry");
+
+
+            
 
             return sb.ToString();
         }
+
+        public string GetTranslations()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Dictionary.Additional.Settlements.Account.{food}.{accoutNameWithDot}\t{accoutDesc}\t{accoutDesc}\t{accoutDesc}\t{accoutDesc}\t{accoutDesc}");
+            sb.AppendLine($"Dictionary.Additional.Settlements.TextOnInvoice.{food}.{accoutNameWithDot}.1\t{textOnInvose}\t{textOnInvose}\t{textOnInvose}\t{textOnInvose}\t{textOnInvose}");
+        }
     }
 }
+
+
+       
+ 
