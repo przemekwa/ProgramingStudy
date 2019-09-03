0ae769fe28056ebca27ed03a4ced6cb1420176c4
Przemyslaw Walkowski <przemyslaw.walkowski@snpgroup.com>
2019-09-03 17:27:04 +0200

    add executeAttribute

---
+++ ProgramingStudy/Study/ExecuteAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramingStudy.Study
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ExecuteAttribute : Attribute
+    {
+        public string DateTime {get;set;}
+
+        public DateTime ExecuteDateTime
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(DateTime) == false)
+                {
+                    return System.DateTime.ParseExact(this.DateTime, "dd-MM-yyyy HH:mm" , CultureInfo.InvariantCulture);
+                }
+
+                return System.DateTime.Now.AddYears(-2);
+            }
+        }
+    }
+}
